2970ae030b6daef5533133fe64e6c295c827f18e
Yohebyth <177250779+Yohebyth@users.noreply.github.com>
2024-08-26 16:24:36 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let Imprimir = "";
+  Imprimir += who[Math.floor(Math.random() * 4)];
+  Imprimir += " " + action[Math.floor(Math.random() * 4)];
+  Imprimir += " " + what[Math.floor(Math.random() * 3)];
+  Imprimir += " " + when[Math.floor(Math.random() * 5)];
+  [Math.floor(Math.random() * 4)];
+  document.querySelector("#excuse").innerHTML = Imprimir;
   console.log("Hello Rigo from the console!");
 };
