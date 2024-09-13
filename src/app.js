edc0e7b2860c20a6ea0299b105e5e206c0d8e76b
Yohebyth <177250779+Yohebyth@users.noreply.github.com>
2024-09-13 22:00:11 +0000

    Se agregan comentarios

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ window.onload = () => {
     "during my lunch",
     "while I was praying"
   ];
+  // Decloro el string imprimir, en el se guardan todas las partes de la excusa, se usa += para conservar el valor actual y agregar el valor nuevo
   let Imprimir = "";
-  Imprimir += who[Math.floor(Math.random() * 4)];
-  Imprimir += " " + action[Math.floor(Math.random() * 4)];
-  Imprimir += " " + what[Math.floor(Math.random() * 3)];
-  Imprimir += " " + when[Math.floor(Math.random() * 5)];
-  [Math.floor(Math.random() * 4)];
-  document.querySelector("#excuse").innerHTML = Imprimir;
-  console.log("Hello Rigo from the console!");
+  // En los pasos siguientes se usa un random para generar la posicion de cada arreglo segun la cantidad de lapabras que tenga y se guarda en imprimir
+  Imprimir += who[Math.floor(Math.random() * 4)]; // El arreglo who en la posicion que genere el random se guarda en imprimir
+  Imprimir += " " + action[Math.floor(Math.random() * 4)]; //El arreglo action en la posicion que genere el random se guarda en imprimir
+  Imprimir += " " + what[Math.floor(Math.random() * 3)]; //El arreglo what en la posicion que genere el random se guarda en imprimir
+  Imprimir += " " + when[Math.floor(Math.random() * 5)]; //El arreglo when en la posicion que genere el random se guarda en imprimir
+  document.querySelector("#excuse").innerHTML = Imprimir; // cambia la palabra excuse en el HTML por la excusa generada
 };
